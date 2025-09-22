66e822e200dd6751dd05831378428f92dc752a2f
Niklas Fischer <niklasfischer90@gmail.com>
2025-09-22 13:59:03 +0200

    Art Assets start

---
+++ JamSeptember2025/Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    float _speed = 5f;
+
+    Vector2 inputMove;
+    InputAction _inputActionMove;
+
+
+    void Start()
+    {
+        _inputActionMove = InputSystem.actions.FindAction("Move");
+    }
+
+    void Update()
+    {
+        inputMove = _inputActionMove.ReadValue<Vector2>();
+        Vector3 moveVector = (Vector3)inputMove.normalized * _speed * Time.deltaTime;
+        transform.position += moveVector;
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # September2025
-A short game jam 
+
+A short game jam
+
+
+
+\[] MVP
+
+\[] Player movement
+
+\[] Player Jump
+
+\[] Player Shoot
+
+\[] Trow Weapon
+
+\[] Take Damage
+
+\[] Local Multiplayer
+
+\[] Level Design
+
+\[] Camera that follows all players on screen
+
+\[] Game Manager
+
+\[] Sandwich Game Start - End Loop
+
+\[] First playtest
+
