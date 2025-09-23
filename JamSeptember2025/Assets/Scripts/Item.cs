60ecff2e1c40133bbdbade058b42ec0896a93d9c
OnHike <elmeras29@gmail.com>
2025-09-23 14:33:18 +0200

    Fisrt Item commit

--- JamSeptember2025/Assets/Scripts/Item.cs
+++ JamSeptember2025/Assets/Scripts/Item.cs
@@ -1,8 +1,12 @@
+using System.Linq;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Item : MonoBehaviour
 {
     // Item class handle interaction and pickup
+
+    public bool owned;
 
     public void SetPosition(Transform newTransform)
     {
@@ -12,12 +16,26 @@ public class Item : MonoBehaviour
 
     public void Use()
     {
-
+        print("used item");
     }
 
     public void Drop()
     {
+        print("Dropped item");
+    }
 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        ItemHandler handler = collision.GetComponent<ItemHandler>();
+        if (handler == null) { return; }
+        handler.items.Add(this);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        ItemHandler handler = collision.GetComponent<ItemHandler>();
+        if (handler == null) { return; }
+        handler.items.Remove(this);
     }
 
 }
